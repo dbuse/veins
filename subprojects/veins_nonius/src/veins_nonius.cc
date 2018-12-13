03564365fcad9cfed6e35ef6e78ed9a2559b9d27
Dominik S. Buse <buse@ccs-labs.org>
2018-12-13 17:17:14 +0100

    Add nonius benchmark subproject.

---
+++ subprojects/veins_nonius/src/veins_nonius.cc
@@ -0,0 +1,4 @@
+// This builds the main binary that will execute all benchmarks.
+// Do not modify this file. Rather, add new .cc files in this directory.
+#define NONIUS_RUNNER
+#include "nonius/nonius.h++"
